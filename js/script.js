bc94423bb05ff00e32071f6b18bd7b22eb715ac5
Yuriy Baranov <yuri.baranov83@gmail.com>
2022-12-01 13:05:51 +0200

    added .places to script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ $(document).ready (function () {
    $('.cards').slick({
       infinite: true,
       slidesToShow : 6
-      
    }
+   );
+});
 
+$(document).ready (function () {
+   $('.places').slick({
+      // infinite: true,
+      slidesToShow : 1
+   }
    );
 });
